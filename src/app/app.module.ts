4f206043a27273f6ffce5d492e9ea15f81a4be4a
radagast666 <ecampos853@gmail.com>
2020-11-23 14:39:30 -0600

    componente #2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MujerIMCComponent } from './mujer-imc/mujer-imc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MujerIMCComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mujer-imc/mujer-imc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mujer-imc',
+  templateUrl: './mujer-imc.component.html',
+  styleUrls: ['./mujer-imc.component.css']
+})
+export class MujerIMCComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
